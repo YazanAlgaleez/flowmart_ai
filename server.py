aa1dd02cbe9fde0124882ab575c69cb04c56fdf0
YAZAN ALGALEEZ <yazanalgaleez25062005@gmail.com>
2025-12-24 17:13:21 +0300

    Update server logic with debug info

--- server.py
+++ server.py
@@ -92,3 +92,4 @@ def recommend_products(user_id: str):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8085))
     uvicorn.run(app, host="0.0.0.0", port=port)
+    # Final Update for Render Logic
